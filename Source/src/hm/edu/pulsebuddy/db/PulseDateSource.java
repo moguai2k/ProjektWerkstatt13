bb58d8c1529bc1a9f36117daca0fc147be0d3acd
moguai2k <moguai2k@me.com>
2013-11-20 19:53:28 +0100

    Simple pulse miner.

---
+++ Source/src/hm/edu/pulsebuddy/db/PulseDateSource.java
@@ -0,0 +1,71 @@
+package hm.edu.pulsebuddy.db;
+
+import java.util.Date;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class PulseDateSource
+{
+
+  private SQLiteDatabase database;
+  private DbOpenHelper dbHelper;
+  private String[] allColumns = { DbOpenHelper.COL_ID,
+      DbOpenHelper.COL_ADDED_ON, DbOpenHelper.COL_PULSE };
+
+  public PulseDateSource( Context context )
+  {
+    dbHelper = new DbOpenHelper( context );
+  }
+
+  public void open() throws SQLException
+  {
+    database = dbHelper.getWritableDatabase();
+  }
+
+  public void close()
+  {
+    dbHelper.close();
+  }
+
+  public PulseModel createPulse( int pulse )
+  {
+    ContentValues values = new ContentValues();
+    values.put( DbOpenHelper.COL_PULSE, pulse );
+
+    long insertId = database.insert( DbOpenHelper.TABLE_NAME, null, values );
+
+    Cursor cursor = database.rawQuery( "SELECT _id AS _id,"
+        + " (strftime('%s', added_on) * 1000) AS added_on," + " pulse"
+        + " FROM pulse WHERE _id = '" + insertId + "'", new String[ 0 ] );
+
+    cursor.moveToFirst();
+
+    PulseModel pulseM = cursorToPulseModel( cursor );
+    cursor.close();
+    return pulseM;
+  }
+
+  private PulseModel cursorToPulseModel( Cursor cursor )
+  {
+    PulseModel pulse = new PulseModel();
+
+    pulse.setId( cursor.getLong( 0 ) );
+
+    long millis = cursor.getLong( cursor.getColumnIndexOrThrow( "added_on" ) );
+    Date addedOn = new Date( millis );
+    pulse.setDateTime( addedOn );
+
+    pulse.setPulse( cursor.getInt( 2 ) );
+
+    Log.d( "DB output", "Pulse: " + pulse.getPulse() + ", date: "
+        + pulse.getDateTime().toString() );
+
+    return pulse;
+  }
+
+}
---
+++ Source/src/hm/edu/pulsebuddy/db/PulseModel.java
@@ -0,0 +1,41 @@
+package hm.edu.pulsebuddy.db;
+
+import java.util.Date;
+
+public class PulseModel
+{
+
+  private long id;
+  private int pulse;
+  private Date dateTime;
+
+  public long getId()
+  {
+    return id;
+  }
+
+  public void setId( long id )
+  {
+    this.id = id;
+  }
+
+  public Date getDateTime()
+  {
+    return dateTime;
+  }
+
+  public void setDateTime( Date dateTime )
+  {
+    this.dateTime = dateTime;
+  }
+
+  public int getPulse()
+  {
+    return pulse;
+  }
+
+  public void setPulse( int pulse )
+  {
+    this.pulse = pulse;
+  }
+}

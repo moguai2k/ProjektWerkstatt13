f22696a928cedfe497eaeb087d6985b54563ee68
eusi <eusi.cf@gmail.com>
2013-12-02 23:49:13 +0100

    added some math-classes, modified colors.xml and strings.xml

---
+++ Source/src/hm/edu/pulsebuddy/math/CalorieCalc.java
@@ -0,0 +1,29 @@
+package hm.edu.pulsebuddy.math;
+
+public class CalorieCalc {
+
+	
+	public void addCalories(int calories) {
+		//db put calories to today + timestamp
+	}
+	
+	public void getCalories() {
+		//db gimme calories all day long
+	}
+	
+	public void caloriesTrafficLight(int food){
+	        switch (food) {
+	            case 1:  addCalories(100);
+	                     break;
+	            case 2:  addCalories(250);
+	                     break;
+	            case 3:  addCalories(500);
+	                     break;
+	            case 4:  addCalories(750);
+	                     break;
+	            case 5:  addCalories(1000);
+	                     break;
+	        }
+	}
+	
+}
---
+++ Source/src/hm/edu/pulsebuddy/math/TrainingSchedule.java
@@ -0,0 +1,5 @@
+package hm.edu.pulsebuddy.math;
+
+public class TrainingSchedule {
+
+}

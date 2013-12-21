48a621cd3ffab1fbfa82026800b1ceb384d57b0b
josefbichlmeier <josefbichlmeier@192.168.1.104>
2013-12-21 18:40:28 +0100

    Add basic stuff for sport mode

---
+++ Source/src/hm/edu/pulsebuddy/SportTestFragment.java
@@ -0,0 +1,36 @@
+package hm.edu.pulsebuddy;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A dummy fragment representing a section of the app, but that simply displays
+ * dummy text.
+ */
+public class SportTestFragment extends Fragment
+{
+  /**
+   * The fragment argument representing the section number for this fragment.
+   */
+  public static final String ARG_SECTION_NUMBER = "section_number";
+
+  public SportTestFragment()
+  {
+  }
+
+  @Override
+  public View onCreateView( LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState )
+  {
+    View rootView = inflater.inflate( R.layout.fragment_sport_test, container,
+        false );
+    // TODO add content
+    return rootView;
+  }
+}

8414be5910903c88acaa6ff3e002f94467bca509
eusi <eusi.cf@gmail.com>
2013-12-25 11:19:17 +0100

    ConconiTest.java (Math) extended

--- Source/src/hm/edu/pulsebuddy/math/ConconiTest.java
+++ Source/src/hm/edu/pulsebuddy/math/ConconiTest.java
@@ -10,7 +10,7 @@ public class ConconiTest
    * @param gender
    * @return
    */
-  public int getStandardHeartRate( int age, Gender gender )
+  public int getStandardMaxHeartRate( int age, Gender gender )
   {
     if ( gender.equals( Gender.male ) )
       return 220 - age;
@@ -18,6 +18,45 @@ public class ConconiTest
       return 226 - age;
     else
       return 0; // error
+  }
+  
+  /**
+   * 
+   * @param pulse
+   * @return
+   */
+  public int getDeflectionPoint( int[] pulse )
+  {    
+    for( int i=0; i < pulse.length; i++ ) {
+      double rate = ( pulse[i+1] - pulse[i] ) *100 / pulse[i];
+      int rateIndex = i;
+      
+      if(i>2 && rate < 1.0)
+        return pulse[rateIndex];
+    }
+    return 0;
+  }
+  
+  /**
+   * 
+   * @param pulse
+   * @return
+   */
+  public int getDeflectionPointFinally( int[] pulse )
+  {
+    double rate = 0.0;
+    int rateIndex = 0;
+    
+    for( int i=0; i < pulse.length; i++ ) {
+      double newRate = ( pulse[i+1] - pulse[i] ) *100 / pulse[i];
+      if(i==0)
+        rate = newRate;
+      else if(newRate < rate) {
+        rate = newRate;
+        rateIndex = i;
+      }
+    }
+    return pulse[rateIndex];
   }
 
 }

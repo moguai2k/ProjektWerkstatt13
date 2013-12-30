47db8bb94556302a16e3cabba7b61abb9953f447
josefbichlmeier <josefbichlmeier@192.168.16.158>
2013-12-30 09:55:08 +0100

    Add custom toast to sport test.

--- Source/src/hm/edu/pulsebuddy/SportTestResultFragment.java
+++ Source/src/hm/edu/pulsebuddy/SportTestResultFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A dummy fragment representing a section of the app, but that simply displays
@@ -20,6 +22,7 @@ public class SportTestResultFragment extends Fragment implements
    */
   public static final String ARG_SECTION_NUMBER = "section_number";
   private View view;
+  private View layout;
 
   public SportTestResultFragment()
   {
@@ -31,6 +34,9 @@ public class SportTestResultFragment extends Fragment implements
   {
     view = inflater.inflate( R.layout.fragment_sport_test_result, container,
         false );
+
+    layout = inflater.inflate( R.layout.custom_toast,
+        (ViewGroup) view.findViewById( R.id.toast_layout ) );
 
     Button buttonAgain = (Button) view.findViewById( R.id.buttonAgain );
     buttonAgain.setOnClickListener( this );
@@ -64,30 +70,25 @@ public class SportTestResultFragment extends Fragment implements
   {
     // TODO @Tore save value in DB
 
-    // customToast( getResources().getString( R.string.common_saved_values ) );
+    customToast( getResources().getString( R.string.common_saved_values ) );
 
   }
 
-  // /**
-  // * Notifies the user that the data has been stored in the database.
-  // *
-  // * @param text
-  // * The message.
-  // */
-  // private void customToast( String text )
-  // {
-  //
-  // LayoutInflater inflater = getLayoutInflater();
-  // View layout = inflater.inflate( R.layout.custom_toast,
-  // (ViewGroup) findViewById( R.id.toast_layout ) );
-  //
-  // TextView textV = (TextView) layout.findViewById( R.id.toastText );
-  // textV.setText( text );
-  //
-  // Toast toast = new Toast( getApplicationContext() );
-  // toast.setDuration( Toast.LENGTH_LONG );
-  // toast.setView( layout );
-  // toast.show();
-  // }
+  /**
+   * Notifies the user that the data has been stored in the database.
+   * 
+   * @param text
+   *          The message.
+   */
+  private void customToast( String text )
+  {
+    TextView textV = (TextView) layout.findViewById( R.id.toastText );
+    textV.setText( text );
+
+    Toast toast = new Toast( getActivity() );
+    toast.setDuration( Toast.LENGTH_LONG );
+    toast.setView( layout );
+    toast.show();
+  }
 
 }
